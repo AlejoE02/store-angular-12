cb4ebd50fd3cffb3d19993bb4b6dec25cabe08dd
alejoe02 <alejoespi1007@gmail.com>
2024-01-22 12:20:31 -0800

    phase I of angular 12 store project

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,63 @@
+import {Component, signal} from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
+import { CommonModule } from "@angular/common";
+import { Product } from "../../../shared/models/product.model";
+import { HeaderComponent } from "../../../shared/components/header/header.component";
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, ProductComponent, HeaderComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  products = signal<Product[]>([]);
+  cart = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=21'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 50,
+        image: 'https://picsum.photos/640/640?r=22'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 4',
+        price: 50,
+        image: 'https://picsum.photos/640/640?r=24'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 5',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=25'
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 6',
+        price: 50,
+        image: 'https://picsum.photos/640/640?r=26'
+      }
+    ];
+    this.products.set(initProducts);
+  }
+  addToCart(product: Product){
+    console.log('we are in the father');
+    this.cart.update(prevState => [...prevState, product]);
+  }
+}
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, signal, SimpleChanges} from '@angular/core';
+import {Product} from "../../models/product.model";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  total = signal(0)
+  @Input({required: true}) cart: Product[] = [];
+
+  toogleSideMenu(){
+    this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    const cart = changes['cart'];
+    if(cart){
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal(){
+    return this.cart.reduce((total, product) => total + product.price, 0)
+  }
+
+}
